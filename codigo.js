e37efd46224eeb269c0888cf04a6eeb81b345413
brunogmatias <bruno.gabriel0589@gmail.com>
2024-09-07 20:44:38 -0300

    mudei o código js

--- codigo.js
+++ codigo.js
@@ -47,15 +47,15 @@ function verificar(){
                 //idoso
                 img.setAttribute('src','imagens/idosa.png')
             }
-            if(Number(fano.value) == 0 || Number(fano.value) == ano){
-                window.alert('Error: adicone o ano de nascimento!')
-            }else{
-            txtp.innerHTML = (`Detectamos ${genero} com ${idade} anos.`)
-            res.appendChild(img)
-        }   
+            
     }    
-        
-}
     
+}   
+}
+if(Number(fano.value) == 0 || Number(fano.value) == ano){
+    window.alert('Error: adicone o ano de nascimento!')
+}else{
+txtp.innerHTML = (`Detectamos ${genero} com ${idade} anos.`)
+res.appendChild(img)
     
 }
